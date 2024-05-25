1cc5db6a6b91d031cebf6ce67de4533b37437af7
aosang <wangle2071@163.com>
2024-05-25 12:00:59 +0800

    v202405251200

---
+++ app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+import Sidebar from './components/Sidebar'
+
+const inter = Inter({ subsets: ['latin'], display: 'swap' })
+
+export const metadata: Metadata = {
+  title: 'Spotify Clone',
+  description: 'Listen to music!',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Sidebar>
+          {children}
+        </Sidebar>
+      </body>
+    </html>
+  )
+}
